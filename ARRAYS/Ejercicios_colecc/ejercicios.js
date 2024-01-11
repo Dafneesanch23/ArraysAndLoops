90d5c128bf4a34a64a45690fa7886e88fe3a18a2
Dafneesanch23 <daaphsanchez@gmail.com>
2024-01-11 13:35:45 -0600

    Se resuelve ejercicio de estatus de usuarios

--- ARRAYS/Ejercicios_colecc/ejercicios.js
+++ ARRAYS/Ejercicios_colecc/ejercicios.js
@@ -63,3 +63,58 @@ function sumaResi(Resistencias) {
 // - `secretName(["Esperanza", "Franco", "Nia"])` should return `'EFN'`.
 // - `secretName(['Phoebe', 'Ross', 'Chandler', 'Joey', 'Monica', 'Rachel'])` should return `'CJMPRR'`.
 // - `secretName(['Harry', 'Ron', 'Hermione'])` should return `'HRH'`.
+
+
+// Online status
+
+ // Display online status for a list of users.
+ // Example:
+// - `onlineStatus(['mockIng99', 'J0eyPunch', 'glassedFer'])` should return `'mockIng99, J0eyPunch and 1 more online'`.
+ 
+
+function userStatus(names) {
+    const totalUsers = names.length;
+if (totalUsers === 1){
+return `${users[0]} is online`;
+} else {
+    const lastUser = names.pop(); 
+    const listUsers = names.join(', ');
+
+    return `${listUsers} and 1 more online`;
+  }
+}
+
+const users = (['mockIng99', 'J0eyPunch', 'glassedFer']);
+const resultado = userStatus(users);
+console.log (resultado);
+
+// Array of Multiples
+ 
+// Create a function that takes two parameters (number, length) and returns an array of length containing multiples of the number.
+ 
+// Examples:
+// - `arrayMultiplos(2, 10)` should return `[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]`.
+// - `arrayMultiplos(17, 6)` should return `[17, 34, 51, 68, 85, 102]`.
+ 
+
+// Positive dominance in Array
+ 
+// Write a function to determine if an array is positively dominant.
+// An array is positively dominant when the majority of its elements are positive.
+ 
+
+// Example:
+// - `positiveDom([-1, -3, -5, 4, 6767])` should return `false`.
+ 
+
+// Antipodal Average
+ 
+
+// Given an array, return a shorter array following these steps:
+// - Split the array into two equal parts*. If unequal, remove the middle element to obtain two equal arrays.
+// - Sum each number of the first part with the inverse numbers of the second part.
+// - Divide each number of the resulting array by 2.
+ 
+
+// Example:
+// - For the array `[1,2,3,5,22,6]`, the result should be `[3.5, 12, 4]`.
